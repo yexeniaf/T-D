b0e0cbcb37bd2bc338aac558a99c156662424d42
Yexenia Flores <yexeniaflores@yexenias-mbp.myfiosgateway.com>
2021-12-14 21:25:35 -0500

    content now shows

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,23 @@
+const DOMAIN = 'https://www.thecocktaildb.com';
 const drinkDiv = document.querySelector("#cocktail-result");
 
 async function fetchData(drink) {
   try {
     const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`;
+    
+    
+    
+    
+    
     const res = await axios.get(url);
     const drinkData = res.data;
-    console.log(drinkData);
-    drinkData.forEach((drinkObj) => {
-      showDrinkData(drinkObj);
-    })
+    // console.log(drinkData);
+    // drinkData.forEach((drinkObj) => {
+    //   showDrinkData(drinkObj[0]);
+    // })
     showDrinkData(drinkData);
   } catch (error) {
-    displayErrorMessage();
+    // displayErrorMessage();
   }
 }
 
@@ -36,8 +42,8 @@ placeHolder();
 function showDrinkData(data) {
   console.log(data);
 
-  const drinkName = document.querySelector("")
-  drinkName.innerText = `${data.drinks.srtDrink}`;
+  const drinkName = document.querySelector("#cocktail-result")
+  drinkName.innerText = `${data.drinks[0].strDrink}`;
   drinkDiv.appendChild(drinkName);
 
   const drinkImg = document.createElement("img");
@@ -62,106 +68,8 @@ function handleSubmit(event) {
   removedrink();
 }
 
+
 function removedrink() {
+ 
   drinkDiv.innerHTML = "";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const cocktailDiv = document.querySelector("#cocktail-Result");
-
-
-// async function fetchData(cocktail) {
-//     try{
-//         const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`;
-//         const res = await axios.get(url);
-//         const cocktailData = res.data;
-//         console.log(cocktailData);
-//         cocktailData.forEach((cocktailObj) => {
-//             showCocktailData(cocktailObj);       
-//         })
-//     }catch (error) {
-//         let h1 = document.createElement("h1");
-//               h1.innerText = "Not Found";
-//               document.body.appendChild(h1);
-//         fetchData("");
-        
-//     }
-// }
-
-// function showCocktailData(data) {
-//     console.log(data);
-  
-    
-//     const cocktailName = document.createElement("h2");
-//     cocktailName.innerText = `${data.drinks.strDrink} ${data.strImageSource}`;
-//     cocktailDiv.appendChild(cocktailName);
-  
-//     const cocktailImg = document.createElement("img");
-//     cocktailImg.src = data.strImageSource.jpg;
-//     console.dir(cocktailImg);
-//     cocktailImg.alt = `Image of ${data.drinks.strDrink}`;
-//     cocktailDiv.appendChild(cocktailImg);
-  
-//   }
-  
-//   const searchForm = document.querySelector("#cocktail-form");
-//   const searchInput = document.querySelector("#cocktail-search");
-  
-//   searchForm.addEventListener("submit", handleSubmit);
-  
-//   function handleSubmit(event) {
-//     event.preventDefault();
-//     console.log(searchInput.value);
-//     let inputValue = searchInput.value;
-//     searchInput.value = "";
-//     console.log(inputValue);
-//     fetchData(inputValue);
-//     removeCountry();
-// }
-
-// function removeCountry() {
-
-//     cocktailDiv.innerHTML = "";
-//   } 
-
-//   console.log(axios);
